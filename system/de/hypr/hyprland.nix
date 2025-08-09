f451aebba5ba440dc49db09c5d2e55228086cd0f
javif89 <javier0eduardo@hotmail.com>
2025-08-09 14:02:09 -0400

    Remove waybar

--- system/de/hypr/hyprland.nix
+++ system/de/hypr/hyprland.nix
@@ -8,7 +8,6 @@
 {
   # Install hyprland and all the stuff it needs
   programs.hyprland.enable = true;
-  programs.waybar.enable = true;
   environment.sessionVariables.NIXOS_OZON_WL = "1";
 
   environment.systemPackages = with pkgs; [
