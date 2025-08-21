fd3e5f781fa484b1eb11200dc80ee4ba90ddbbb2
Javier Feliz <javier0eduardo@hotmail.com>
2025-08-21 00:29:50 -0400

    Steam stuff

--- modules/system/gaming.nix
+++ modules/system/gaming.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   # Install Steam
@@ -8,4 +8,8 @@
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
   };
+
+  environment.systemPackages = with pkgs; [
+    gamemode
+  ];
 }
